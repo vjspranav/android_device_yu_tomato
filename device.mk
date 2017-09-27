f14763f353f9eca4fa73ef1f0d34808696515398
vjspranav <pranavasri@live.in>
2017-09-28 01:48:30 +0530

    Update Path

--- device.mk
+++ device.mk
@@ -29,6 +29,6 @@ include $(LOCAL_PATH)/product/*.mk
 
 # Call the proprietary setup
 $(call inherit-product, vendor/yu/tomato/tomato-vendor.mk)
-$(call inherit-product-if-exists, vendor/volte/volte.mk)
-$(call inherit-product-if-exists, vendor/volte/tomato/tomato-vendor.mk)
+$(call inherit-product-if-exists, vendor/yu/volte.mk)
+$(call inherit-product-if-exists, vendor/yu/tomato/tomato-vendor.mk)
 
