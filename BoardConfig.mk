2e42bf1482a78efdae63e53645b04fef64e0e821
Rajesh <rajeshramece@gmail.com>
2019-04-20 14:07:01 +0530

    migrate lineagehw to java overlays

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 
 # LineageHW
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # Power
 TARGET_POWER_SET_FEATURE_LIB := libpower_set_feature_tomato
